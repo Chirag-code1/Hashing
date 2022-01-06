5050a649183b2407deb4b0bf168af6846f230571
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-06 23:43:13 +0530

    Count frequency of each character
    Given a string that contains only lowercase characters. Write a program to print all the distinct characters along with their frequency in the order of their occurrence. For example if the string is “helloworld”, then you should print h1 e1 l3 o2 w1 r1 d1
    
    Complete the given function countFrequency() and print the frequency of each character as per the given statement.

---
+++ Count frequency of each character.cpp
@@ -0,0 +1,13 @@
+void countFrequency(string str){
+  int count[26] = {0};
+    for(int i=0; i<str.length(); i++)
+        count[str[i]-'a']++;
+    for(int i=0; i<str.length(); i++)
+    {
+        if(count[str[i]-'a'] != 0)
+        {
+            cout << str[i] << count[str[i]-'a'] << " ";
+            count[str[i]-'a'] = 0;
+        }
+    }
+}
