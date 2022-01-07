3f431a20f498f47182eaf2b0771c30a2237705dc
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-07 12:53:04 +0530

    Minimum indexed character in strings

---
+++ Minimum indexed character in strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+char minIndexChar(string str1, string str2){
+  set <char> set;
+  map<int, char> mp;
+    for(int i =0 ; i< str2.length(); i++)
+      set.insert(str2[i]);
+    for(int i=0 ; i<str1.length(); i++){
+        if(set.find(str1[i])!=set.end()){
+            mp.insert({i+1, str1[i]});
+        }
+    }
+    auto p = mp.begin()->second;
+    return p;
+}
