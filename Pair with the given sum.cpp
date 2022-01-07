07925fc2ec6d60789d0ee3ac80f7749d2ee2c09d
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-07 12:56:43 +0530

    Pair with the given sum
    Given a sorted array of n elements, and an integer k. Find whether there exists any two elements in the array that sums up to the given value k. If such a pair is found, then print “Found”, else print “Not Found”.
    
    You may not use the same element twice.

---
+++ Pair with the given sum.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+bool pairSum(vector <int> arr, int n, int k){
+  set<int> hashSet;
+    for (int i=0; i<n; i++)
+    {
+ 	   if (hashSet.find(k - arr[i]) != hashSet.end())
+ 	       return true;
+ 	   hashSet.insert(arr[i]);
+    }
+    return false;
+}
