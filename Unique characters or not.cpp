0406b515777d9112ae79c8518ca975332aa4247f
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-07 12:58:01 +0530

    Unique characters or not

---
+++ Unique characters or not.cpp
@@ -0,0 +1,22 @@
+// Return true if string contains all unique characters, else return false
+#include<algorithm>
+#include<string>
+bool isUniqueChars(string str){
+  int n = str.length();
+  sort(str.begin(), str.end());
+  for(int i=0;i<n-1;i++){
+    if(str[i] == str[i+1])
+      return false;
+  }
+  return true;
+  //Using Count Array:
+      // int count[256] = {0};
+    //   for(int i=0; i< str.length(); i++){
+    //       count[str[i]]++;
+    //   }
+    //   for(int i=0;i<str.length();i++){
+    //       if(count[str[i]] > 1)
+    //       return false;
+    //   }
+    //   return true;
+}
