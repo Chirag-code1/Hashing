586b6f04c4cb1fc14fbf5935b19174900a7b9c1b
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-08 13:46:03 +0530

    Create String is pangram or not.cpp

---
+++ String is pangram or not.cpp
@@ -0,0 +1,19 @@
+/*     
+ * Complete the 'isPangrams' function below.
+ * @params
+ *  str -> a string that is to be tested for pangram
+ *
+ * @return
+ *  boolean value, based on the fact, whether 
+ *  the given string is a pangram or not.
+ */
+bool isPangram(string str) {
+  int count[26] = {0};
+  for(int i=0; i< str.length(); i++)
+  count[str[i]-'a']++;
+  for(int i=0; i< 26;i++){
+      if(count[i] == 0)
+      return false;
+  }
+  return true;
+}
