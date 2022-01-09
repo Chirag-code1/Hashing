b0c6166864a17cb7b381c301eb5435ab13dc10ed
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-09 13:49:32 +0530

    Create Find contiguous sub-array with given sum.cpp

---
+++ Find contiguous sub-array with given sum.cpp
@@ -0,0 +1,20 @@
+void findTheSubArray(int arr[], int n, int sum){
+	map<int,int> map;
+    int prefixSum = 0;
+    for(int i=0; i< n ; i++){
+        prefixSum = prefixSum + arr[i];
+        if(prefixSum == sum){
+            cout<<0<<" "<<i<<endl;
+            return;
+        }
+        else {
+            if(map.find(prefixSum - sum)!=map.end()){
+                cout<<map[prefixSum-sum]+1<<" "<<i<<endl;
+                return;
+            } else {
+                map[prefixSum] = i;
+            }
+        }
+    }
+    cout<<-1<<endl;
+}
